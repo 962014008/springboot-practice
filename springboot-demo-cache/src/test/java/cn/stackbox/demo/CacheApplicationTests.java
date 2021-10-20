1ef2745565ee3f73f526a2f6eb559164191d3e4a
962014008 <962014008@qq.com>
2021-10-20 22:42:18 +0800

    #问题#

--- springboot-demo-cache/src/test/java/cn/stackbox/demo/CacheApplicationTests.java
+++ springboot-demo-cache/src/test/java/cn/stackbox/demo/CacheApplicationTests.java
@@ -35,10 +35,14 @@ public class CacheApplicationTests {
 			@Override
 			public Integer call() throws Exception {
 				// get请求
+				// 1. 测试Ehcache缓存cache1 (使用KeyGenerator)
+				// 问题：缓存生效的时候，ehcache里面存储的数据是怎么样的？
 				MockHttpServletRequestBuilder get1 = MockMvcRequestBuilders.get("/cache/cache1");
 				String result1 = mockMvc.perform(get1).andReturn().getResponse().getContentAsString();
 				System.out.println("get /cache/cache1 请求结果: " + result1);
 
+				// 2. 测试Ehcache缓存cache2 (使用自定义Key)
+				// 问题：缓存生效的时候，ehcache里面存储的数据是怎么样的？
 				MockHttpServletRequestBuilder get2 = MockMvcRequestBuilders.get("/cache/cache2");
 				String result2 = mockMvc.perform(get2).andReturn().getResponse().getContentAsString();
 				System.out.println("get /cache/cache2 请求结果: " + result2);
@@ -50,14 +54,18 @@ public class CacheApplicationTests {
 			@Override
 			public Integer call() throws Exception {
 				// get请求
+				// 3. 测试Redis缓存cache3
+				// 问题：缓存生效的时候，redis里面存储的数据是怎么样的？
 				MockHttpServletRequestBuilder get3 = MockMvcRequestBuilders.get("/cache/cache3");
 				String result3 = mockMvc.perform(get3).andReturn().getResponse().getContentAsString();
 				System.out.println("get /cache/cache3 请求结果: " + result3);
 
+				// 4. 刷新缓存
+				// 问题：刷新缓存后，再次调用缓存，返回什么数据？
 				MockHttpServletRequestBuilder get4 = MockMvcRequestBuilders.get("/cache/refresh");
 				String result4 = mockMvc.perform(get4).andReturn().getResponse().getContentAsString();
-				System.out.println("get /cache/cache2 请求结果: " + result4);
-
+				System.out.println("get /cache/refresh 请求结果: " + result4);
+				
 				return 1;
 			}}.call();
 
